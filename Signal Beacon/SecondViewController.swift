85c40dff0ca40636bbea987d9897d82a521818aa
David <dgallub1@gmail.com>
2017-11-04 15:37:24 -0400

    Initial Commit

---
+++ Signal Beacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Signal Beacon
+//
+//  Created by David Gallub on 11/4/17.
+//  Copyright © 2017 Amatrine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
