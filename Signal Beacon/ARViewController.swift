1bac630177977c5a89693ca2c07cd6597fac6bf2
David <dgallub1@gmail.com>
2017-11-04 21:56:21 -0400

    More Progress on AR: Changed to ARSK (renders 2D objects) and allowed for placing labels

--- Signal Beacon/ARViewController.swift
+++ Signal Beacon/ARViewController.swift
@@ -7,11 +7,12 @@
 //
 
 import ARKit
+import SpriteKit
 import Firebase
 
-class  ARViewController: UIViewController, ARSCNViewDelegate {
+class  ARViewController: UIViewController, ARSKViewDelegate {
     
-    @IBOutlet weak var ARSceneView: ARSCNView!
+    @IBOutlet weak var sceneView: ARSKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +23,60 @@ class  ARViewController: UIViewController, ARSCNViewDelegate {
         })
         
         // Set up ARSceneView
-        ARSceneView.delegate = self
-        ARSceneView.showsStatistics = true
+        sceneView.delegate = self
+        
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        
+        let scene = SKScene(size: sceneView.bounds.size)
+        scene.scaleMode = .resizeFill
+        sceneView.presentScene(scene)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create and run the ARSceneView session
+        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        ARSceneView.session.run(configuration)
+        configuration.worldAlignment = .gravityAndHeading
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the session when page is closed
-        ARSceneView.session.pause()
+        // Pause the view's session
+        sceneView.session.pause()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Create anchor using the camera's current position
+        if let currentFrame = sceneView.session.currentFrame {
+            // Create a transform with a translation of 0.2 meters in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.2
+            let transform = simd_mul(currentFrame.camera.transform, translation)
+            
+            // Add a new anchor to the session
+            let anchor = ARAnchor(transform: transform)
+            sceneView.session.add(anchor: anchor)
+        }
+    }
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        // Create and configure a node for the anchor added to the view's
+        // session.
+        let labelNode = SKLabelNode(text: "Hello, Thomas")
+        labelNode.fontSize = 6
+        labelNode.fontColor = UIColor.green
+        labelNode.horizontalAlignmentMode = .center
+        labelNode.verticalAlignmentMode = .center
+        return labelNode;
+    }
+    
+    
     
     @IBAction func backButtonPressed(_ sender: Any) {
     }
